d317b639ee7aa093221d6b23e9352549428ea672
Papa Kofi <humblepapa1234@gmail.com>
2023-07-20 15:03:10 +0000

    creating a pop up menu and Bottom Navigation Menu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,35 +10,92 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   double? _deviceHeight;
+  List<PopupMenuEntry<String>> options = [
+    const PopupMenuItem<String>(
+      value: 'item 1',
+      child: Text('Item 1'),
+    ),
+  ];
+  String? selectedOption;
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    Container(
+      child: const Center(
+        child: Text('Feed'),
+      ),
+    ),
+    Container(
+      child: const Center(
+        child: Text('Profile'),
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: _deviceHeight! * 0.08,
-        title: const Text(
-          'Our Gallery',
-          style: TextStyle(
-              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        backgroundColor: themeColors[0],
-        actions: [
-          IconButton(
-              iconSize: _deviceHeight! * 0.03,
-              onPressed: () {},
-              icon: const Icon(
-                Icons.add_a_photo,
+          toolbarHeight: _deviceHeight! * 0.08,
+          title: const Text(
+            'Our Gallery',
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          backgroundColor: themeColors[0],
+          actions: [
+            IconButton(
+                iconSize: _deviceHeight! * 0.03,
+                tooltip: 'Add a photo',
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add_a_photo,
+                  color: Colors.white,
+                )),
+            IconButton(
+                iconSize: _deviceHeight! * 0.03,
+                tooltip: 'log out',
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                )),
+            PopupMenuButton<String>(
                 color: Colors.white,
-              )),
-          IconButton(
-              iconSize: _deviceHeight! * 0.03,
-              onPressed: () {},
-              icon: const Icon(
-                Icons.logout,
-                color: Colors.white,
-              )),
-        ],
-      ),
+                iconSize: _deviceHeight! * 0.03,
+                initialValue: selectedOption,
+                tooltip: 'More options',
+                onSelected: (String item) {
+                  setState(() {
+                    selectedOption = item;
+                  });
+                },
+                itemBuilder: (BuildContext context) {
+                  return options;
+                }),
+          ]),
+      bottomNavigationBar: _bottomNavBar(),
+      body: _pages[_selectedIndex],
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _bottomNavBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.feed), label: 'Feed'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_box), label: 'Profile'),
+      ],
+      backgroundColor: themeColors[0],
+      selectedItemColor: Colors.white,
+      onTap: _onItemTapped,
+      currentIndex: _selectedIndex,
     );
   }
 }
